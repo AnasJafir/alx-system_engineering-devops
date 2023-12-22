a02123d4dab560fa258a8881188e3056577932ac
AnasJafir <dr.anas.jafir@example.com>
2023-12-22 15:59:16 -0800

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,16 @@
-# install puppet-lint -v 2.5.0
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Define the package name and version
+$package_name = 'Flask'
+$desired_version = '2.1.0'
+
+# Ensure pip is installed
+package { 'python3-pip':
+  ensure => installed,
 }
+
+# Install Flask with the specific version using pip
+exec { "install_flask_${desired_version}":
+  command => "/usr/bin/pip3 install ${package_name}==${desired_version}",
+  path    => ['/usr/bin'],
+  unless  => "/usr/bin/python3 -m flask --version | grep ${desired_version}",
+}
+
