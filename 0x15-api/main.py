bd51178322e472ec0e50f98c16f312a9908eefe6
AnasJafir <dr.anas.jafir@gmail.com>
2024-03-25 18:19:13 -0700

    from API to JSON format

--- 0x15-api/main.py
+++ 0x15-api/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-Check student .CSV output of user information
+Check student JSON output
 """
 
-import csv
+import json
 import requests
 import sys
 
@@ -12,26 +12,33 @@ todos_url = "https://jsonplaceholder.typicode.com/todos"
 
 
 def user_info(id):
-    """ Check CSV formatting """
+    """ Check user info """
+    
+    with open(str(id) + '.json', 'r') as f:
+        student_json = json.load(f)
 
+    student_dicts = student_json[str(id)]
+    
     response = requests.get(todos_url).json()
-    with open(str(id) + ".csv", 'r') as f:
-        output = f.read().strip()
-        count = 0
-        flag = 0
-        for i in response:
-            if i['userId'] == id:
-                url = users_url + str(i['userId'])
-                usr_resp = requests.get(url).json()
-                line = '"' + str(i['userId']) + '","' + usr_resp[0]['username'] + '","' + str(i['completed']) + '","' + i['title'] + '"'
-                count += 1
-                if not line in output:
-                    print("Task {} Formatting: Incorrect".format(count))
+    flag = 0
+    json_count = 0
+    not_found_count = 0
+    for i in response:
+        if i['userId'] == id:
+            usr_resp = requests.get(users_url + str(i['userId'])).json()
+            json_entry = {'username': usr_resp[0]['username'], 'completed': i['completed'], 'task': i['title']}
+            json_count += 1
+            flag = 0
+            for item in student_dicts:
+                if json_entry == item:
                     flag = 1
+            if flag == 0:
+                not_found_count += 1
 
-    if flag == 0:
-        print("Formatting: OK")
-
+    if not_found_count != 0:
+        print("Number of tasks missing: {}".format(not_found_count))
+    else:
+        print("All tasks found: OK")
 
 if __name__ == "__main__":
     user_info(int(sys.argv[1]))
