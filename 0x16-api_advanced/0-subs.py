14ca87052cedbd3b2dc2c8d53fe690422af6f51e
AnasJafir <dr.anas.jafir@gmail.com>
2024-04-11 18:36:06 +0000

    Total subscribers for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,11 @@ def number_of_subscribers(subreddit):
     """Method that return the number of subs"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'My-User-Agent'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     
-    if response.status_code == 200:
+    if response.status_code >= 300:
+	return 0
+    else:
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
-    else:
-        return 0
